940eac85569a4ec6095cfcbb195c8ff6ec64b698
PlanetLesus <105377434+PlanetLesus@users.noreply.github.com>
2022-05-11 20:44:43 +0200

    Blöcke

---
+++ src/BLOCK.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BLOCK here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BLOCK extends Actor
+{
+    /**
+     * Referenzattribute
+     */
+    int x, y;
+    // speicherplatz für grafik
+    // methode zum item machen
+    public BLOCK(int x,int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    /**
+     * Act - do whatever the BLOCK wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ src/PFÜTZE.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PFÜTZE here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PFÜTZE extends BLOCK
+{
+    PFÜTZE()
+    {
+        super();
+    }
+    /**
+     * Act - do whatever the PFÜTZE wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
