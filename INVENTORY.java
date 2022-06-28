60954f8c3d7775b8a54c21e1adabf0c266209348
haennes <hannsesh.sharemail@gmail.com>
2022-06-28 20:58:22 +0200

    selecting inventoryslot

--- INVENTORY.java
+++ INVENTORY.java
@@ -64,6 +64,9 @@ public class INVENTORY
     }
     
     public void update_view(){
+        for(INVENTORYSLOT slot: slots){
+            slot.setImage(background_image);
+        }
         slots[selected_slot].setImage(background_image_selected);
     }
         
